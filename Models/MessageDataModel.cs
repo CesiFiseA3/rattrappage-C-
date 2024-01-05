c080f442f62c3f3030f5b6763fb021a82b75792f
Jules MORILLON <jules.morillon@viacesi.fr>
2024-01-05 11:20:22 +0100

    main viewmodel + messagedatamodel+ filedatamodel

--- Models/MessageDataModel.cs
+++ Models/MessageDataModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,8 @@ namespace Rattrapage_Programmation_Système.Models
 {
     internal class MessageDataModel
     {
+        public int id { get; set; }
+        public string content { get; set; }
+        public List<FileDataModel> attachedFiles { get; set; } 
     }
 }
